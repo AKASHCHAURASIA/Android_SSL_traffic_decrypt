cd95ffa050fb4bd3620a8e8ba2d86cbf4b53ccc2
akash.chaurasia@bobble.ai <akash.chaurasia@bobble.ai>
2021-01-02 20:31:29 +0530

    added

---
+++ attach.py
@@ -0,0 +1,33 @@
+import frida
+import time
+import sys
+import os
+def on_message(message, data):
+
+    if message['type'] == 'send':
+        message_payload = []
+        message_payload = message['payload']
+        
+
+        print('---------',message_payload)
+
+
+device = frida.get_usb_device()
+
+
+#pid = device.spawn(["com.test.myapplication"])
+
+#device.resume(pid)
+
+time.sleep(1)  # Without it Java.perform silently fails
+
+session = device.attach("com.bitstrips.imoji")
+
+script = session.create_script(open(sys.argv[2]).read())
+
+script.on('message', on_message)
+
+script.load()
+
+
+t=sys.stdin.read()
